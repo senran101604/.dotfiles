8fb68eb8834e9956c22842078ff0078e1669f5cc
SenRan <senran101604@gmail.com>
2024-01-13 00:22:56 +0800

    Initial Commit

---
+++ devilspie2/.config/devilspie2/auto-move.lua
@@ -0,0 +1,7 @@
+if (get_application_name() == "Alacritty") then
+    set_window_workspace(1)
+elseif (get_application_name() == "org.pwmt.zathura") then
+    set_window_workspace(2)
+elseif (get_application_name() == "Firefox") then
+    set_window_workspace(3)
+end
