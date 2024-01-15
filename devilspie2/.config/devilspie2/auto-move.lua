184dd1128d9d8a9c6776c387ea331bd6c9d9bb19
SenRan <senran101604@gmail.com>
2024-01-15 15:16:05 +0800

    update

--- devilspie2/.config/devilspie2/auto-move.lua
+++ devilspie2/.config/devilspie2/auto-move.lua
@@ -1,7 +1,9 @@
 if (get_application_name() == "Alacritty") then
-    set_window_workspace(1)
+    set_window_workspace(7)
+elseif (get_application_name() == "kitty") then
+    set_window_workspace(7)
 elseif (get_application_name() == "org.pwmt.zathura") then
-    set_window_workspace(2)
+    set_window_workspace(6)
 elseif (get_application_name() == "Firefox") then
-    set_window_workspace(3)
+    set_window_workspace(1)
 end
